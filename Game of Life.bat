b880ecf17dc9dbf5954b72a67561f57b3250ae31
Laith Shono <lshono8@gmail.com>
2020-09-18 18:35:56 +0300

    🐛 fixed a bug in batch file

--- Game of Life.bat
+++ Game of Life.bat
@@ -1,1 +1,1 @@
-java --module-path "c:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls -jar "Game of Life.jar"
+java --module-path "lib\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls -jar "Game of Life.jar"
